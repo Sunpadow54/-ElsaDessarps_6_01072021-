90b04e60dd202dddb7d32a17ead77638e28f65af
elsa <elsa.dessarps@gmail.com>
2021-07-05 14:48:13 +0200

    Feat (db) :  add file to connect to Mongodb ( use "dotenv" mod ; to connect need an .env file with parameters : DB_URI, DB_NAME, DB_USER, DB_PASS )

---
+++ backend/db.js
@@ -0,0 +1,34 @@
+// Database connection
+
+// ------------------------- IMPORTS -------------------------
+
+const db = require('mongoose'); // Mongoose package to interact with mongoDB
+const dotEnv = require('dotenv'); // DotEnv, module used for Db password, username ... 
+
+
+// ============================================================
+// ----- Create connexion to Mongodb Database
+
+const dbConnect = () => {
+    dotEnv.config() // invoking the dotenv config here
+
+    const uri = process.env.DB_URI;
+   
+    db.connect(uri, {
+           dbName: process.env.DB_NAME,
+           user: process.env.DB_USER,
+           pass: process.env.DB_PASS,
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           useFindAndModify: false,
+           useCreateIndex: true
+       })
+            .then(() => console.log('Connexion à MongoDb réussie !'))
+            .catch((error => console.log('Connexion à mongoBD échouée ! ' + error.message)));
+}
+
+
+// ============================================================
+// ------------------------- EXPORT ---------------------------
+
+module.exports = dbConnect;
